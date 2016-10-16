729e743bbd780a8295e3013ff4a3dc19a9aaafcf
Jesse Williams <jess@workwoo.com>
2016-10-15 20:09:45 -0700

    Initial commit

---
+++ orca-ai.js
@@ -0,0 +1,24 @@
+var log = {
+	info: function(string) {
+		console.log('INFO: ' + string);
+	},
+
+	error: function(string) {
+		console.log('ERROR: ' + string);
+	},
+
+	object: function(object){
+		console.log('OBJECT: ')
+		for(var property in object) {
+			console.log('    { "' + property + '": "' + object[property] + '" }');
+		}
+	},
+}
+
+// Database & Storage
+var mongoose = require('mongoose');
+var aws = require('aws-sdk');
+
+log.info('hello');
+log.error('Error');
+log.object(person);
